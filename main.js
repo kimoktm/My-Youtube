4e14e09065d0ecaa1be4e8209c48fa3c9f473281
Karim <cg.karimtarek@gmail.com>
2015-02-19 14:35:25 +0200

    AutoHide video Controls

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 var searchInput = document.getElementById('masthead-search-term');
 searchInput.setAttribute('placeholder', "Search here");
+
+var moviePlayer = document.getElementById('movie_player');
+moviePlayer.className  = moviePlayer.className + " ideal-aspect";
