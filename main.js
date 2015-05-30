18ee16ad859940cf30975c2a441ed90c0dd62293
kimoktm <cg.karimtarek@gmail.com>
2015-05-30 17:09:47 +0200

    update

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-var searchInput = document.getElementById('masthead-search-term');
-searchInput.setAttribute('placeholder', "Search here");
+// Add Search Placeholder
+searchPlaceHolder();
 
-var moviePlayer = document.getElementById('movie_player');
-moviePlayer.className  = moviePlayer.className + " ideal-aspect";
+// Hide Video Controls
+// hideControls();
+
+// Hide controls across site
+document.body.onclick = timeOutHide;
+
+
+function timeOutHide() {
+	// setTimeout(function(){ hideControls(); }, 4000);
+}
+
+function hideControls() {
+	var moviePlayer = document.getElementById('movie_player');
+	if (typeof(moviePlayer) != 'undefined' && moviePlayer != null) {
+		moviePlayer.className  = moviePlayer.className + " ideal-aspect";
+	}
+}
+
+function searchPlaceHolder() {
+	var searchInput = document.getElementById('masthead-search-term');
+	if (typeof(searchInput) != 'undefined' && searchInput != null) {
+		searchInput.setAttribute('placeholder', "Search here");
+	}
+}
